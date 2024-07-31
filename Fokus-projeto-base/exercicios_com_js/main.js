c95a2eca64d71788925d24685d5506eba3162cee
João Victor <81127942+joaovictor-jpg@users.noreply.github.com>
2024-07-31 20:21:46 -0300

    Trocando o fundo e a imagem com ação no js

---
+++ Fokus-projeto-base/exercicios_com_js/main.js
@@ -0,0 +1,15 @@
+const htmlTreino = document.querySelector('html');
+const cronometoTreino = document.querySelector('.app__card-timer');
+const imagemTreino = document.querySelector('.app__image');
+const tituloTreino = document.querySelector('.app__title');
+const focusBtTreino = document.querySelector('.app__card-button--foco');
+const descansoCurtoBtTreino = document.querySelector('.app__card-button--curto');
+const descansoLongoBtTreino = document.querySelector('.app__card-button--longo');
+const temporizadorFocoTreino = 1500;
+const temporizadorDescansoCurtoTreino = 300;
+const temporizadorDescansoLongoTreino = 900;
+
+console.log(htmlTreino)
+console.log(cronometoTreino);
+console.log(imagemTreino);
+console.log(tituloTreino);
