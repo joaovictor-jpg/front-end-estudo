6ae63cd53001cbc0ab5d76f7901ebc0e474dd5a7
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2024-11-06 22:17:09 -0300

    TypeScript e compilador

--- typescript-curso-1-arquivos-iniciais/app/app.ts
+++ typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -1,7 +1,10 @@
 import { Negocicao } from "./models/negociacao.js";
+
 const negociacao = new Negocicao(new Date(), 10, 100);
 console.log(negociacao);
 // negociacao.quantidade = 1000;
 console.log(negociacao);
+
 console.log(negociacao.data);
+
 alert("Hello world");
